b2ce1f33efacc90cbd392ba15dd658f1bda9441d
Pragyesh <pragyeshtiwari29@gmail.com>
2022-09-07 16:07:38 +0530

    Adding 1

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int a, b;
+    cout << "Enter both numbers : ";
+    cin >> a >> b;
+    a = a^b;
+    b = a^b;
+    a = b^a;
+    cout << a << " " << b;
+    return 0;
 }
