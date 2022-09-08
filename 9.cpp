00996db54c6fa03daf52d23f840367561cba16e7
Pragyesh <pragyeshtiwari29@gmail.com>
2022-09-08 09:29:39 +0530

    Adding 9

--- 9.cpp
+++ 9.cpp
@@ -1,20 +1,25 @@
 #include<iostream>
-#include<vector>
+#include<set>
 #include<math.h>
 using namespace std;
 int main(){
     int n;
-    vector<int> res;
+    set<int> res;
     cout << "Enter number : ";
     cin >> n;
+    int r = sqrt(n);
     cout << "Prime factors are : ";
-    if(n % 2 == 0) res.push_back(2);
+    if(n % 2 == 0) res.emplace(2);
     while(n % 2 == 0){
         n /= 2;
     }
-    for(int i = 3; i < sqrt(n); i+=2){
-        if(n % 3 == 0) res.push_back(i);
+    for(int i = 3; i < r; i+=2){
+        if(n % i == 0){
+            n /= i;
+            res.emplace(i);
+        }
     }
+    if(n > 1) res.emplace(n);
     for(int i: res) cout << i << " ";
     return 0;
 }
