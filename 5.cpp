472b0fc9a80e30d3b5bf404f64dbdb35f0f810f8
Pragyesh <pragyeshtiwari29@gmail.com>
2022-09-20 18:21:21 +0530

    Square root using Binary Search

--- 5.cpp
+++ 5.cpp
@@ -1,18 +1,35 @@
 #include<iostream>
-#include<math.h>
-#include<iomanip>
 using namespace std;
 
 double squareRoot(int n){
-    return pow(2.732, log(n)/2);
+    int beg = 0, end = n, mid, precision = 3;
+    double ans = 0.0, increment = 0.1;
+    while(beg <= end){
+        mid = (beg + end) / 2;
+        int sqr = mid * mid;
+        if(sqr == n){
+            return mid;
+        }else if(sqr > n){
+            end = mid - 1;
+        }else{
+            ans = mid;
+            beg = mid + 1;
+        }
+    }
+    for(int i = 0; i < precision; i++){
+        while(ans * ans <= n){
+            ans += increment;
+        }
+        ans -= increment;
+        increment /= 10;
+    }
+    return ans;
 }
 
 int main(){
     int n;
     cout << "Enter Number : ";
     cin >> n;
-    double x = squareRoot(n);
-    if(x - (int)x < 0.1) cout << (int)x;
-    else cout << setprecision(5) << x;
+    cout << squareRoot(n);
     return 0;
 }
