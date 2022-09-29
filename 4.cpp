a2fb708db8eab630e9cad5f5552190216326852b
Pragyesh <pragyeshtiwari29@gmail.com>
2022-09-29 12:09:21 +0530

    Adding 4

--- 4.cpp
+++ 4.cpp
@@ -2,25 +2,33 @@
 #include<math.h>
 using namespace std;
 
+// helper function to calculate factorial.
 long long fact(int n){
     long long f = 1;
     for(int i = 2; i <= n; i++) f *= i;
     return f;
 }
 
-void sine(int x, int n){
+void sine(float x, int n){
+     int toggle = 0;
+     float res = 0.0f;
+     for(int i = 1; i <= n; i+=2){
+        float nmr = pow(-1, toggle) * pow(x, i);
+        float dmr = fact(i);
+        if(toggle) toggle = 0;
+        else toggle = 1;
+        res += (nmr / dmr);
+     }
+     cout << res;
 }
 
 int main(){
-    int x, n;
+    int n;
+    float x;
     cout << "Enter x : ";
     cin >> x;
     cout << "Enter n : ";
     cin >> n;
-    // if(n > 15){
-    //     cout << "n must be strictly positive and less than or equal to 15";
-    //     return 0;
-    // }
     sine(x, n);
     return 0;
 }
